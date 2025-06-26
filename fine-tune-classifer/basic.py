bccc240a73a0496db774f4aa758edfe42bc8334b
jiaxijzhang <jiaxijzhang@global.tencent.com>
2025-06-26 16:30:25 +0000

    Support multi-classes.

--- fine-tune-classifer/basic.py
+++ fine-tune-classifer/basic.py
@@ -17,22 +17,24 @@ from transformers import (
     pipeline,
 )
 
-raw_dataset = load_dataset("/home/jiaxijzhang/llm_relevant_study/dataset/llm_router_dataset-synth")
-print(f"Train dataset size: {len(raw_dataset['train'])}")
-print(f"Test dataset size: {len(raw_dataset['test'])}")
+raw_dataset = load_dataset("dair-ai/emotion", "split")
 
 model_path = "/nfs1/jiaxinzhang/models/ModernBERT-large"
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 tokenizer.model_max_length = 512
 
 def tokenize(batch):
-    return tokenizer(batch["prompt"], padding="max_length", truncation=True, return_tensors="pt")
+    return tokenizer(batch["text"], padding="max_length", truncation=True, return_tensors="pt")
 
 # Tokenize dataset
 raw_dataset =  raw_dataset.rename_column("label", "labels") # to match Trainer
-tokenized_dataset = raw_dataset.map(tokenize, batched=True, remove_columns=["prompt"])
+tokenized_dataset = raw_dataset.map(tokenize, batched=True, remove_columns=["text"])
 
-labels = list(set(tokenized_dataset["train"]["labels"]))
+# label2id, id2label是无所谓的, 和数据中labels无关系,
+# label2id: 不知道干嘛的
+# id2label: 讲预测的id转为label
+# labels = list(set(tokenized_dataset["train"]["labels"]))
+labels = ["sadness", "joy", "anger", "fear", "love", "surprise"]
 num_labels = len(labels)
 label2id, id2label = dict(), dict()
 for i, label in enumerate(labels):
@@ -48,12 +50,12 @@ def compute_metrics(eval_pred):
     predictions, labels = eval_pred
     predictions = np.argmax(predictions, axis=1)
     score = f1_score(
-        labels, predictions, labels=labels, pos_label=1, average="weighted",
+        labels, predictions, average="weighted",
     )
     return {"f1": float(score) if score == 1 else score}
 
 training_args = TrainingArguments(
-    output_dir="/nfs1/jiaxinzhang/saved_for_checkpoint/bert-classifier",
+    output_dir="/nfs1/jiaxinzhang/saved_for_checkpoint/bert-classifier-multi",
     per_device_train_batch_size=64,
     per_device_eval_batch_size=32,
     learning_rate=5e-5,
@@ -68,14 +70,14 @@ training_args = TrainingArguments(
     load_best_model_at_end=True,
     metric_for_best_model="f1",     # must be the name of metric returned by compute_metrics
     report_to="wandb",
-    run_name="bert-classifier-1",
+    run_name="bert-classifier-multi-classes",
 )
 
 trainer = Trainer(
     model=model,
     args=training_args,
     train_dataset=tokenized_dataset["train"],
-    eval_dataset=tokenized_dataset["test"],
+    eval_dataset=tokenized_dataset["validation"],
     compute_metrics=compute_metrics,
 )
 
@@ -84,16 +86,17 @@ trainer = Trainer(
 
 # /nfs1/jiaxinzhang/saved_for_checkpoint/bert-classifier/checkpoint-240
 # load model from huggingface.co/models using our repository id
-tokenizer_path = "/nfs1/jiaxinzhang/saved_for_checkpoint/bert-classifier"
-test_model_path = "/nfs1/jiaxinzhang/saved_for_checkpoint/bert-classifier/checkpoint-480"
+tokenizer_path = "/nfs1/jiaxinzhang/models/ModernBERT-large"
+test_model_path = "/nfs1/jiaxinzhang/saved_for_checkpoint/bert-classifier-multi/checkpoint-250"
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+# 模型已经保存了label2id=label2id, id2label=id2label,
 test_model = AutoModelForSequenceClassification.from_pretrained(
-   test_model_path, num_labels=num_labels, label2id=label2id, id2label=id2label,
+   test_model_path, num_labels=num_labels
 )
 test_model.to("cuda")
 classifier = pipeline("sentiment-analysis", model=test_model, tokenizer=tokenizer, device=0)
 
-sample = "How does the structure and function of plasmodesmata affect cell-to-cell communication and signaling in plant tissues, particularly in response to environmental stresses?"
-
-pred = classifier(sample)
-print(pred)
+sample = raw_dataset["test"][0]
+pred = classifier(sample["text"])
+print(f"Pred: {pred}")
+print(f"Gold: {id2label[sample["labels"]]}")
